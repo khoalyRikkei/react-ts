fae74f42acb3eb437846dc6662857c9da28d8ef4
Khoa Le Rikkei <116610704+khoalyRikkei@users.noreply.github.com>
2023-09-28 09:45:12 +0700

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello bài 10</h1>
       </header>
     </div>
   );
